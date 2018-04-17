4995b3cf0da7cb2fa3f5b439724a480f0c3d80b9
Markus Binsteiner <makkus@posteo.de>
2018-04-17 22:50:00 +1200

    Adding common filters.

--- filter_plugins/freckles_filters.py
+++ filter_plugins/freckles_filters.py
@@ -18,8 +18,38 @@ class FilterModule(object):
             'assemble_package_list': self.assemble_package_list,
             'relative_path': self.relative_path,
             'file_list_filter': self.file_list_filter,
-            'expand_user_remote': self.expand_user_remote
+            'expand_user_remote': self.expand_user_remote,
+            'pretty_list': self.pretty_list,
+            'frklize': self.frklize
         }
+
+    def frklize(self, freckle_vars, var_to_extract, frkl_format):
+
+        result = []
+
+        for folder, folder_metadata in freckles_profile_metadata.items():
+
+            var_list = folder_metadata.get("vars", [])
+            for metadata in var_list:
+                parent_vars = copy.deepcopy(metadata.get("vars", {}))
+                parent_vars.pop(var_to_extract, None)
+
+                packages = metadata.get("vars", {}).get(var_to_extract, [])
+
+                pkg_config = {"vars": parent_vars, var_to_extract: packages}
+
+                chain = [frkl.FrklProcessor(frkl_format)]
+                frkl_obj = frkl.Frkl(pkg_config, chain)
+                pkgs = frkl_obj.process()
+
+                result = result + pkgs
+
+        return sorted(result, key=lambda k: k.get("vars", {}).get("name", "zzz"))
+
+
+    def pretty_list(self, list_to_join, join_token=","):
+
+        return join_token.join(list_to_join)
 
     def expand_user_remote(self, path, user_home):
 
